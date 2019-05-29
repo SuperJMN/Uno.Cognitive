d56edbacc3e3e707fdf1ff92796f144c014c812c
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-05-29 14:23:41 +0200

    Default image

--- Uno.Cognitive/Uno.Cognitive.Shared/ImageLoadingViewModel.cs
+++ Uno.Cognitive/Uno.Cognitive.Shared/ImageLoadingViewModel.cs
@@ -4,6 +4,11 @@ namespace Uno.Cognitive
 {
     public class ImageLoadingViewModel : SectionViewModel
     {
+        public ImageLoadingViewModel()
+        {
+            ImageUrl = "http://orig04.deviantart.net/e05a/f/2016/356/7/7/another_classic_sonic_render_by_alsyouri2001-dasito3.png";
+        }
+
         private string imageUrl;
 
         public string ImageUrl
